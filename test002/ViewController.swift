34b930ba73aa7fe60df060ce32d0018169976006
kt005 <kt005@kt005nomacbook-puro.local>
2017-12-24 21:55:16 +0900

    Initial Commit

---
+++ test002/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  test002
+//
+//  Created by kt005 on 2017/12/24.
+//  Copyright © 2017年 kt005. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
